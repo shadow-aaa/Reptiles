5579ae93a5147765bef25be6dbfb7df21ee6b104
shadow-aaa <1813003722@qq.com>
2024-06-04 21:22:51 +0800

    1

--- getdata.py
+++ getdata.py
@@ -1,14 +1,34 @@
 # -*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
     'Chrome/72.0.3626.109 Safari/537.36'}
 url="http://db.foodmate.net/yingyang/type_1.html"
-response=requests.get(url=url)
-soup=BeautifulSoup(response.text,"html.parser")
-a=soup.find("div",id="dibu")
-for b in a:
-    b=a.find("a")
-    print(b.get("href"))
+def getfoodname():
+    global datalist
+    response=requests.get(url=url)
+    soup=BeautifulSoup(response.text,"html.parser")
+    a=soup.find("div",id="dibu")
+    b=a.find_all("a")
+    datalist=[]
+    for c in b:
+        datalist.append(c.string)
+
+def geturl():
+    response=requests.get(url=url)
+    soup=BeautifulSoup(response.text,"html.parser")
+    a=soup.find("div",id="dibu")
+    b=a.find_all("a")
+    for c in b:
+        print(c.get("href"))
+# geturl()
+datalist=[]
+getfoodname()
+mydict={}
+mytuple=tuple(datalist)
+mydict['test']=mytuple
+with open('./test.csv','w',newline='') as file:
+    writer = csv.DictWriter(file,fieldnames=mydict)
+
