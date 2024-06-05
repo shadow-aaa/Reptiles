9d8bea0ba46d3b8989dfd81ce9bb37c5b87cf342
shadow-aaa <1813003722@qq.com>
2024-06-05 17:52:11 +0800

    50%

--- getdata.py
+++ getdata.py
@@ -24,7 +24,7 @@ def getfoodname(num):
     return foodnamelist
 
 
-def getfoodkind():
+def getfoodkind(num):
     url = f"http://db.foodmate.net/yingyang/type_{num}.html"
     b = getdata(url=url, tag="top")
     kindfood_miss = ['鱼类', '婴儿食品类', '小吃类']
@@ -51,9 +51,28 @@ def geturl(num):
     return urllist
 
 
+def getdatanumber(num):
+    urllist = geturl(num=num)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/72.0.3626.109 Safari/537.36'}
+    numbers=[]
+    for url in urllist:
+        baseurl = "http://db.foodmate.net/yingyang/"
+        url = baseurl+url
+        response = requests.get(url=url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        a = soup.find_all("div", class_="list")
+        for b in a:
+            c=b.find("div",class_="list_m")
+            number=b.text.replace(c.text,'').strip()
+            numbers.append(number)
+    print(numbers)
+        # 应该有嵌套列表
+
+
 def datatocsv(foodkindid, foodkind, foodname):
     global df
-    url = f'http://db.foodmate.net/yingyang/type_{foodkindid}.html'
     temp = pd.DataFrame()
     temp['名字'] = foodname
     temp['分类'] = foodkind[foodkindid-1]
@@ -62,9 +81,10 @@ def datatocsv(foodkindid, foodkind, foodname):
     print(df)
 
 
-df = pd.DataFrame(columns=['名字', '分类'])
-for i in range(2):
-    foodkind = getfoodkind(i+1)
-    foodname = getfoodname(i+1)
-    datatocsv(i+1, foodkind, foodname)
+# df = pd.DataFrame(columns=['名字', '分类'])
+for i in range(1):
+    getdatanumber(i+1)
+#     foodkind = getfoodkind(i+1)
+#     foodname = getfoodname(i+1)
+#     datatocsv(i+1, foodkind, foodname)
 # geturl()
